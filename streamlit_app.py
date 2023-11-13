cb5f402ba9de37408521d86a84ae98872d8c8f33
DorotaJez <101281085+DorotaJez@users.noreply.github.com>
2023-11-13 12:11:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Kale smoothie')
 streamlit.text('Eggs')
 streamlit.text('Avocado toast')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt").set_index('Fruit')
 streamlit.header('Build your own fruit smoothie')
 streamlit.multiselect('Pick some fruits', list(my_fruit_list.index)):
 streamlit.dataframe(my_fruit_list)
